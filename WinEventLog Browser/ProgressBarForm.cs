540bf9bb6a3ef361f9b5bfeb7b78c3685c012e3f
mrle <marko.marich@gmail.com>
2013-10-26 18:17:09 +0200

    Adding the first code implementations
    The program is working but now without bugs. Status line implementation
    started. Threading.

---
+++ WinEventLog Browser/ProgressBarForm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinEventLog_Filter
+{
+    class ProgressBarForm
+    {
+    }
+}
---
+++ WinEventLog Browser/SearchConditions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinEventLog_Filter
+{
+    class SearchConditions
+    {
+        public string[] SearchTerms;
+        public string EventID;
+        public string EventLog;
+        public string EventSource;
+        public string EventType;
+        public DateTime StartDate;
+        public DateTime EndDate;
+        public bool MatchExactEventSource;
+        public bool MissingLinksFiltering;
+        public string MachineIP;
+        public string MachineName;
+
+
+
+        /// <summary>
+        /// Helper method which prints the search condition into resulting text box
+        /// </summary>
+        public string  PrintSearchConditions()
+        {
+            string txtResults;
+
+            txtResults = "MACHINE_NAME: " + MachineName;
+            txtResults += "\r\nMACHINE_IP_ADDRESS: " + MachineIP + "\r\n";
+            txtResults += "\r\n**************************************************";
+            txtResults += "\r\nSEARCH CONDITIONS:";
+            txtResults += "\r\n Search terms:";
+            if (SearchTerms != null)
+            {
+                for (var i = 0; i < SearchTerms.Length; i++)
+                    txtResults += " " + SearchTerms[i] + ",";
+                txtResults = txtResults.TrimEnd(',');
+            }
+            txtResults += "\r\n Event ID: " + EventID;
+            txtResults += "\r\n Event Type: " + EventType;
+            txtResults += "\r\n Event Log: " + EventLog;
+            txtResults += "\r\n Start Date: " + StartDate;
+            txtResults += "\r\n End Date: " + EndDate;
+            txtResults += "\r\n Use missing links filtering: " + (MissingLinksFiltering ? "Yes" : "No");
+            txtResults += "\r\n**************************************************\r\n";
+
+            return txtResults;
+        }
+    }
+}
